c24b2487f410191d405b326b2e1a9c76cbe23559
Miguel <lucas.116349@e.unavarra.es>
2020-05-07 15:28:12 +0000

    Organize repo in directories for jenkins

---
+++ lucas116349Proyecto1/Arduino/build-uno/core/WString.cpp.d
@@ -0,0 +1,3 @@
+build-uno/core/WString.cpp.o: \
+ /home/alumno/Descargas/arduino-1.8.9/hardware/arduino/avr/cores/arduino/WString.cpp \
+ /home/alumno/Descargas/arduino-1.8.9/hardware/arduino/avr/cores/arduino/WString.h
---
+++ lucas116349Proyecto1/Arduino/build-uno/core/wiring_pulse.S.d
@@ -0,0 +1,2 @@
+build-uno/core/wiring_pulse.S.o: \
+ /home/alumno/Descargas/arduino-1.8.9/hardware/arduino/avr/cores/arduino/wiring_pulse.S
